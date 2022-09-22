98bbec7b24a9f104ca94f620e93b11508f353a78
ludvigla <ludvigla@kth.se>
2022-09-22 17:20:49 +0200

    Added function to load image information. The function LoadImageData() resturn an object of class DFrame (S4Vectors) which stores the image together with scalfactors

--- R/load_data.R
+++ R/load_data.R
@@ -6,15 +6,21 @@
 #' This means that if a gene is missing in a certain dataset, the spots in that dataset will
 #' be assigned with 0 expression.
 #'
-#' @family pre-processing
-#' @describeIn load-data
+#' Spot IDs are renamed to be unique. Usually, the spots are named something similar to:
+#' \code{"ACGCCTGACACGCGCT-1", "TACCGATCCAACACTT-1"}
 #'
-#' @param samplefiles Character vector of file/directory paths. Paths should specify .h5 or
-#' .tsv/.tsv.gz files. Alternatively, the paths could specify directories including barcodes.tsv,
-#' features.tsv and matrix.mtx files.
+#' Since spot barcodes are shared across datasets, there is a risk that some of the spot IDs
+#' will be duplicated after merging. To avoid this, the prefix (e.g. "-1") is replaced by
+#' a unique prefix for each loaded matrix: "-1", "-2", "-3", ...
+#'
+#' @family pre-process
+#'
+#' @param samplefiles Character vector of file/directory paths. Paths should specify `.h5` or
+#' `.tsv`/`.tsv.gz` files. Alternatively, the paths could specify directories including `barcodes.tsv`,
+#' `features.tsv` and `matrix.mtx` files.
 #' @param verbose Print messages
 #'
-#' @return A sparse matrix of class 'dgCMatrix'
+#' @return A sparse matrix of class `dgCMatrix`
 #'
 #' @examples
 #' \donttest{
@@ -75,4 +81,127 @@ LoadAndMergeMatrices <- function (
     return(exprMats[[1]])
   }
 
+}
+
+
+#' Load and merge multiple coordinate tables
+#'
+#' Load coordinates from \strong{'tissue_positions_list.csv'} files and merge them into a `tibble`.
+#'
+#' @details The merging process makes sure that all genes detected are present in the merged output.
+#' This means that if a gene is missing in a certain dataset, the spots in that dataset will
+#' be assigned with 0 expression.
+#'
+#' @family pre-process
+#'
+#' @param coordinatefiles Character vector of file paths. Paths should specify `.csv` files output by spaceranger.
+#' @param verbose Print messages
+#'
+#' @return An object of class `tibble`
+#'
+#' @examples
+#' \donttest{
+#' # Load and merge coordinates from two samples
+#' coordinatefiles <- c(system.file("extdata/mousebrain/spatial", "tissue_positions_list.csv", package = "STUtility2"),
+#'              system.file("extdata/mousecolon/spatial", "tissue_positions_list.csv", package = "STUtility2"))
+#' coordinates <- LoadSpatialCoordinates(coordinatefiles)
+#' }
+#'
+#' @export
+LoadSpatialCoordinates <- function (
+    coordinatefiles,
+    verbose = TRUE
+) {
+
+  # Run checks
+  if (!is.character(coordinatefiles)) abort("'coordinatefiles' must be a character vector.")
+  checks <- tibble::tibble(coordinatefiles) |>
+    dplyr::mutate(is = dplyr::case_when(file.exists(coordinatefiles) ~ "file"))
+  if (any(is.na(checks$is))) abort(c("Invalid path(s):", glue::glue("{checks$coordinatefiles[is.na(checks$is)]}")))
+
+  # Load coordinates
+  if (verbose) inform(c("i" = "Loading coordinates:"))
+  coordDF <- inject(rbind(!!!lapply(seq_along(coordinatefiles), function(i) {
+    coords <- read.csv(file = coordinatefiles[i], header = FALSE) |>
+      tibble::as_tibble() |>
+      setNames(nm = c("barcode", "selected", "y", "x", "pxl_col_in_fullres", "pxl_row_in_fullres"))
+    if (verbose) inform(c("v" = glue::glue("  Finished loading coordinates for sample {i}")))
+    coords$barcode <- gsub(pattern = "-\\d+", replacement = paste0("-", i), x = coords$barcode)
+    coords$sampleID <- i
+    return(coords)
+  })))
+
+  # Return coordinates
+  if (verbose) inform(c("v" = glue::glue("  Collected coordinates for {nrow(coordDF)} spots.")))
+  return(coordDF)
+}
+
+#' Read image data
+#'
+#' Load image related data from \strong{'tissue_hires_image.png'} files.
+#'
+#' @family pre-process
+#'
+#' @param images An object of class `tibble` containing paths to images in PNG format, with
+#' one row per sample. Paths should specify `.png` files output by spaceranger such as
+#' `tissue_lowres_image.png` or `tissue_hires_image.png`. You do not have to load both
+#' H&E images.
+#' @param jsonfiles A character vector with file paths. Paths should specify `.json` files containing
+#' scalefactors output by spaceranger.
+#' @param verbose Print messages
+#'
+#' @return An object of class `DFrame``
+#'
+#' @examples
+#' \donttest{
+#' # Collect spaeranger output files
+#' lowres.imagefiles <- c(system.file("extdata/mousebrain/spatial", "tissue_lowres_image.png", package = "STUtility2"),
+#'              system.file("extdata/mousecolon/spatial", "tissue_lowres_image.png", package = "STUtility2"))
+#' hires.imagefiles <- c(system.file("extdata/mousebrain/spatial", "tissue_hires_image.png", package = "STUtility2"),
+#'              system.file("extdata/mousecolon/spatial", "tissue_hires_image.png", package = "STUtility2"))
+#' jsonfiles <- c(system.file("extdata/mousebrain/spatial", "scalefactors_json.json", package = "STUtility2"),
+#'              system.file("extdata/mousecolon/spatial", "scalefactors_json.json", package = "STUtility2"))
+#'
+#' # Load lowres/hires image data for two samples
+#' imgFiles <- tibble::tibble(lowres.imagefiles, hires.imagefiles)
+#' imgData <- LoadImageData(imgFiles, jsonfiles)
+#'
+#' # Load lower image data only
+#' imgFiles <- tibble::tibble(lowres.imagefiles)
+#' imgData <- LoadImageData(imgFiles, jsonfiles)
+#' }
+#' @export
+LoadImageData <- function (
+    images,
+    jsonfiles,
+    verbose = TRUE
+) {
+
+  # Check files
+  check <- sapply(images, file.exists)
+  if (any(!check)) abort(message = c("Invalid path(s):", glue::glue("File path '{unlist(images[!check])}' does not exist")))
+
+  # read image data
+  if (verbose) inform(c("i" = glue::glue("Reading image data for {nrow(images)} samples.")))
+  imgData <- inject(rbind(!!!lapply(1:nrow(images), function(i) {
+    png.files <- unlist(images[i, ])
+    exts <- tools::file_ext(png.files)
+    check <- exts %in% "png"
+    if (!all(check)) abort(glue::glue("Invalid image format: '{exts[!check]}'"))
+    DF <- readImgData(
+      imageSources = unlist(images[i, ]),
+      scaleFactors = jsonfiles[i],
+      sample_id = paste0(i),
+      load = FALSE
+    )
+    if (verbose)
+      sapply(basename(unlist(images[i, ])), function(impath) {
+        inform(c(
+          "v" = glue::glue("  Finished reading '{impath}' image data for sample {i}")
+        ))
+      })
+    return(DF)
+  })))
+
+  return(imgData)
 }
