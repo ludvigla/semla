e9b178dd46039c6d7aea7da3b17bc36bbfa1a16a
ludvigla <ludvigla@kth.se>
2022-11-24 16:01:54 +0100

    roxygen2 docs for external data

--- R/extdata.R
+++ R/extdata.R
@@ -2,7 +2,7 @@
 #'
 #' A Visium dataset obtained from a coronal tissue section of a mouse brain.
 #' \itemize{
-#'    \item{\strong{mousebrain/filtered_feature_bc_matrix.h5}:\cr gene expression matrix in hdf5 format filtered to include spots under the tissue}
+#'    \item{\strong{mousebrain/filtered_feature_bc_matrix.h5}:\cr light weight gene expression matrix with 188 genes in hdf5 format filtered to include spots under the tissue}
 #'    \item{\strong{mousebrain/spatial/tissue_lowres_image.png}: \cr H&E image (600x565) pixels}
 #'    \item{\strong{mousebrain/spatial/tissue_hires_image.png}: \cr H&E image (2000x1882) pixels}
 #'    \item{\strong{mousebrain/spatial/tissue_positions_list.csv}: \cr CSV file with spot coordinates}
@@ -18,7 +18,7 @@ NULL
 #'
 #' A Visium dataset obtained from a "swiss roll" of a mouse colon
 #' \itemize{
-#'    \item{\strong{mousecolon/filtered_feature_bc_matrix.h5}:\cr gene expression matrix in hdf5 format filtered to include spots under the tissue}
+#'    \item{\strong{mousecolon/filtered_feature_bc_matrix.h5}:\cr light weight gene expression matrix with 188 genes in hdf5 format filtered to include spots under the tissue}
 #'    \item{\strong{mousecolon/spatial/tissue_lowres_image.png}: \cr H&E image (600x541) pixels}
 #'    \item{\strong{mousecolon/spatial/tissue_hires_image.png}: \cr H&E image (2000x1804) pixels}
 #'    \item{\strong{mousecolon/spatial/tissue_positions_list.csv}: \cr CSV file with spot coordinates}
