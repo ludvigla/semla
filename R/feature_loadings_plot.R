6e66c144faa45cc74af088b0ad7ebe0980c7f0bd
ludvigla <ludvigla@kth.se>
2023-01-25 09:43:54 +0100

    Added PlotFeatureLoadings and docs

---
+++ R/feature_loadings_plot.R
@@ -0,0 +1,210 @@
+#' Plot feature loadings for dimensional reduction data
+#'
+#' This function can be used to visualize the relative contribution of features
+#' to dimensionality reduction vectors. The function provides two modes to draw either
+#' bar plot or a dot plot.
+#'
+#' @section Select type:
+#' For centered dimensionality reduction vectors, such as principal components,
+#' it is best to select features with the highest and lowest loadings. For other types
+#' of dimensionality reduction results, it is better to only select the features with
+#' the highest loadings, in which case `type="positive"` is the appropriate choice.
+#'
+#' @section Select mode:
+#' barplots or dotplots can be used to get detailed information about the loadings
+#' for individual factors whereas heatmap is useful to summarize the loadings for
+#' multiple factors.
+#'
+#' @param object An object of class `Seurat`
+#' @param dims An integer vector of dimensions to plot feature loadings for
+#' @param reduction A character specifying the dimensionality reduction to use
+#' @param nfeatures Number of features to show
+#' @param mode Plot mode. One of "barplot", "dotplot" or "heatmap"
+#' @param type Mode used to select features:
+#' \itemize{
+#'    \item{"positive" : select features with highest loadings}
+#'    \item{"negative" : select features with lowest loadings}
+#'    \item{"centered" : split selection of features to include top and bottom loadings}
+#' }
+#' @param fill Fill color for barplot and dotplot
+#' @param color Border color for barplot and dotplot
+#' @param bar_width With of barplot provided as a proportion
+#' @param pt_size Size of points in dotplot
+#' @param pt_stroke Width of border for points in dotplot
+#' @param linetype Select a linetype to be used for dotplot, e.g. "solid", "longdash" or "blank"
+#' @param color_by_loadings Should the fill color of barplot or points reflect the feature loadings?
+#' @param gradient_colors Colors to use for gradient if `color_by_loadings=TRUE`
+#' @param ncols Number of columns used for final patchwork
+#'
+#' @import dplyr
+#' @import rlang
+#' @importFrom tidyr pivot_longer
+#' @importFrom tibble as_tibble rownames_to_column
+#' @importFrom patchwork wrap_plots
+#'
+#' @return An object of class `patchwork`
+#'
+#' @examples {
+#' library(STUtility2)
+#'
+#' se_mbrain <- readRDS(system.file("extdata/mousebrain", "se_mbrain", package = "STUtility2"))
+#'
+#' # Run PCA
+#' se_mbrain <- se_mbrain |>
+#'                 ScaleData() |>
+#'                 RunPCA()
+#'
+#' # Plot feature loadings
+#' PlotFeatureLoadings(se_mbrain, reduction = "pca",
+#'                     mode = "dotplot", type = "centered")
+#' }
+#'
+#' @export
+PlotFeatureLoadings <- function (
+  object,
+  dims = 1,
+  reduction = "pca",
+  nfeatures = 30,
+  mode = c("dotplot", "barplot", "heatmap"),
+  type = c("positive", "negative", "centered"),
+  fill = "lightgrey",
+  color = "black",
+  bar_width = 0.9,
+  pt_size = 4,
+  pt_stroke = 0.5,
+  linetype = "dashed",
+  color_by_loadings = FALSE,
+  gradient_colors = RColorBrewer::brewer.pal(n = 9, name = "Reds"),
+  ncols = NULL
+) {
+
+  # Run checks
+  mode <- match.arg(mode, choices = c("dotplot", "barplot", "heatmap"))
+  type <- match.arg(type, choices = c("positive", "negative", "centered"))
+  stopifnot(inherits(dims, what = c("numeric", "integer")),
+            length(dims) > 0,
+            inherits(nfeatures, what = c("numeric", "integer")),
+            nfeatures > 1)
+
+  # Fetch data from Seurat object
+  dimred_data_raw <- object[[reduction]]@feature.loadings[, dims, drop = FALSE]
+  dimred_names <- colnames(dimred_data_raw)
+
+  dimred_data <- dimred_data_raw |>
+    as.data.frame() |>
+    rownames_to_column(var = "name") |>
+    as_tibble() |>
+    pivot_longer(all_of(dimred_names), names_to = "dim", values_to = "value")
+
+  if (mode != "heatmap") {
+
+    # Select features based on type
+    if (type == "centered") {
+      top_n <- ceiling(nfeatures/2)
+      bottom_n <- nfeatures - top_n
+      dimred_data_top <- dimred_data |>
+        group_by(dim) |>
+        arrange(-value) |>
+        slice_head(n = top_n)
+      dimred_data_bottom <- dimred_data |>
+        group_by(dim) |>
+        arrange(value) |>
+        slice_head(n = bottom_n)
+      dimred_data <- bind_rows(dimred_data_top, dimred_data_bottom)
+    } else {
+      if (type == "positive") {
+        dimred_data <- dimred_data |>
+          arrange(-value)
+      }
+      if (type == "negative") {
+        gradient_colors <- gradient_colors |> rev()
+        dimred_data <- dimred_data |>
+          arrange(value)
+      }
+      dimred_data <- dimred_data |>
+        group_by(dim) |>
+        slice_head(n = nfeatures)
+    }
+
+    # Rearrange values
+    dimred_data_split <- dimred_data |>
+      arrange(value) |>
+      group_by(dim) |>
+      group_split() |>
+      setNames(nm = dimred_names)
+
+    plots <- lapply(dimred_names, function(nm) {
+      cur_data <- dimred_data_split[[nm]] |>
+        mutate(name = factor(name, levels = name))
+      p <- ggplot()
+      if (mode == "barplot") {
+        p <- p +
+          {
+            if (color_by_loadings) {
+              geom_col(data = cur_data,
+                       aes(name, value, fill = value), color = color, width = bar_width)
+            } else {
+              geom_col(data = cur_data,
+                       aes(name, value), fill = fill, color = color, width = bar_width)
+            }
+          }
+      }
+      if (mode == "dotplot") {
+        p <- p  +
+          geom_segment(data = cur_data,
+                       aes(x = name, xend = name, y = 0, yend = value),
+                       linetype = linetype) +
+          {
+            if (color_by_loadings) {
+              geom_point(data = cur_data,
+                         aes(name, value, fill = value),
+                         shape = 21, size = pt_size, color = color, stroke = pt_stroke)
+            } else {
+              geom_point(data = cur_data,
+                         aes(name, value),
+                         shape = 21, fill = fill, size = pt_size, color = color, stroke = pt_stroke)
+            }
+          }
+      }
+      p <- p +
+        coord_flip() +
+        theme_minimal() +
+        labs(y = "Feature loading", x = "Feature name", title = nm) +
+        theme(axis.text = element_text(colour = "black"))
+      return(p)
+    })
+
+    ncols <- ncols %||% ceiling(sqrt(length(plots)))
+    p <- wrap_plots(plots, ncol = ncols)
+
+    # Add color scale if data should be colored by loadings
+    if (color_by_loadings) {
+      p <- p &
+        scale_fill_gradientn(colours = gradient_colors) &
+        labs(fill = "Feature\nloading", y = "")
+    }
+  } else {
+    stopifnot(length(dims) > 1)
+    top_features <- dimred_data |>
+      group_by(dim) |>
+      arrange(-value) |>
+      slice_head(n = nfeatures) |>
+      pull(name) |>
+      unique() |>
+      rev()
+    # Subset dimred data
+    dimred_data_subset <- dimred_data |>
+      group_by(dim) |>
+      mutate(value_scaled = scales::rescale(value)) |>
+      filter(name %in% top_features) |>
+      mutate(name = factor(name, levels = top_features))
+
+    p <- ggplot(dimred_data_subset, aes(dim, name, fill = value_scaled)) +
+      geom_tile() +
+      scale_fill_gradientn(colours = gradient_colors) +
+      theme(panel.background = element_blank(), axis.text = element_text(colour = "black")) +
+      labs(x = "", y = "Feature name", fill = "scaled\nFeature\nLoading")
+  }
+
+  return(p)
+}
